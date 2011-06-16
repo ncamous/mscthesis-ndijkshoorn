c18050cb9c6f6c0e9f4736f17d85a03e7748bd92
nickemanz <nickemanz@d4a371cc-1ec7-9fdb-5528-915157eee335>
2011-06-16 13:04:35 +0000

    KF filter for x position seems to work

--- cpp/ardrone_slam/ardrone_slam/slam_module_sensor.cpp
+++ cpp/ardrone_slam/ardrone_slam/slam_module_sensor.cpp
@@ -1,12 +1,14 @@
 #include "global.h"
 #include "slam_module_sensor.h"
 #include "bot_ardrone.h"
+#include "opencv_helpers.h"
 
 using namespace cv;
 
 
 slam_module_sensor::slam_module_sensor(slam *controller):
-	KF(3, 3, 0),
+	KF(3, 1, 0),
+	//KF(3, 3, 0),
 	state(3, 1, CV_32F),
 	processNoise(3, 1, CV_32F)
 {
@@ -15,19 +17,21 @@ slam_module_sensor::slam_module_sensor(slam *controller):
 	prev_update = clock();
 
 	// measure accel
-	measurement = Mat::zeros(3, 1, CV_32F);
+	measurement = Mat::zeros(1, 1, CV_32F);
+	//measurement = Mat::zeros(3, 1, CV_32F);
 
-	//randn( state, Scalar::all(0), Scalar::all(0.1) );
+	// F vector
 	KF.transitionMatrix = *(Mat_<float>(3, 3) << 1, 1, 0, 0, 1, 1, 0, 0, 1);
 
+	// H vector
+	KF.measurementMatrix =  *(Mat_<float>(1, 3) << 0, 0, 1);
 	//setIdentity(KF.measurementMatrix);
-	KF.measurementMatrix = *(Mat_<float>(3, 3) << 1, 1, 0, 0, 1, 1, 0, 0, 1);
 
 	setIdentity(KF.processNoiseCov, Scalar::all(1e-5));
-	setIdentity(KF.measurementNoiseCov, Scalar::all(1e-2));
-	setIdentity(KF.errorCovPost, Scalar::all(0.4));
+	setIdentity(KF.measurementNoiseCov, Scalar::all(1e-5));
+	setIdentity(KF.errorCovPost, Scalar::all(1));
 
-	randn(state, Scalar::all(0), Scalar::all(0.01) );
+	// random initial state
 	randn(KF.statePost, Scalar::all(0), Scalar::all(0.01));
 }
 
@@ -39,36 +43,44 @@ slam_module_sensor::~slam_module_sensor(void)
 
 void slam_module_sensor::process(bot_ardrone_measurement *m)
 {
-
-
-	/* update transition matrix */
-	float difftime = float(((double)clock() - prev_update) / CLOCKS_PER_SEC);
-	KF.transitionMatrix.at<float>(0, 1) = difftime;
-	KF.transitionMatrix.at<float>(1, 2) = difftime;
-
-	KF.measurementMatrix.at<float>(0, 1) = difftime;
-	KF.measurementMatrix.at<float>(1, 2) = difftime;
-
+	double difftime = ((double)clock() - prev_update) / CLOCKS_PER_SEC;
 	prev_update = clock();
 
+	/* update transition matrix */
+	// position
+	KF.transitionMatrix.at<float>(0, 1) = float(difftime);
+	KF.transitionMatrix.at<float>(0, 2) = float(0.5 * difftime*difftime);
+	// velocity
+	KF.transitionMatrix.at<float>(1, 2) = float(difftime);
 
 
+	/* predict */
 	Mat prediction = KF.predict();
 
 
-	// generate measurement
-	state.at<float>(2) = m->accel[0] * MG_TO_MS2;
-	measurement = KF.measurementMatrix*state;
+	/* correct */
+	measurement.at<float>(0) = m->accel[0] * MG_TO_MS2;
+	//measurement = KF.statePost.clone();
+	//measurement.at<float>(2) = m->accel[0] * MG_TO_MS2;
+	//measurement = KF.transitionMatrix*measurement;
 
 	KF.correct(measurement);
 
 
-	randn( processNoise, Scalar(0), Scalar::all(sqrt(KF.processNoiseCov.at<float>(0, 0))));
-	state = KF.transitionMatrix*state + processNoise;
+	/* state */
+	//randn( processNoise, Scalar(0), Scalar::all(sqrt(KF.processNoiseCov.at<float>(0, 0))));
+	//state = KF.transitionMatrix*state + processNoise;
+
+	//randn( processNoise, Scalar(0), Scalar::all(sqrt(KF.processNoiseCov.at<float>(0, 0))));
+	state = KF.statePost /* + processNoise*/;
 
 
 	printf("state: [%f, %f, %f]\n", state.at<float>(0), state.at<float>(1), state.at<float>(2));
-	printf("gt: [%f]\n", m->gt_loc[0]);
+	printf("gt:    [%f]\n", m->gt_loc[0]);
+}
+
+
+
 
 
 
@@ -121,4 +133,3 @@ void slam_module_sensor::process(bot_ardrone_measurement *m)
 
 	//int t[3] = {last_loc[0], last_loc[1], elevation};
 	//matlab->add_elevation_map_tuple((int*) t);
-}
