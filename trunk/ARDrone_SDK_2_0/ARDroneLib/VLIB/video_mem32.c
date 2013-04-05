1b51ea8bdd28ce9a33ecf6b344a8697c50ece989
physar@gmail.com <physar@gmail.com@d4a371cc-1ec7-9fdb-5528-915157eee335>
2013-04-05 18:26:47 +0000

    IranOpen2013: port towards ARDrone_SDK_2_0 (Part three)
    * Connect for 64bits Release to USARSim
    * Will do it in parts, this part is VLIB (129 files)

---
+++ trunk/ARDrone_SDK_2_0/ARDroneLib/VLIB/video_mem32.c
@@ -0,0 +1,54 @@
+#include <VLIB/video_mem32.h>
+#include <VLIB/Platform/video_utils.h>
+
+#include <VP_Os/vp_os_types.h>
+
+#ifndef bswap
+// from http://www.klocwork.com/products/documentation/current/Checkers:PORTING.BSWAP.MACRO
+#if BIG_ENDIAN
+#define SWAPBYTES(x) ( ((x) & 0xff) << 24 ) | ( (((x) >> 8) & 0xff) << 16 ) | ( (((x) >> 16) & 0xff) << 8 ) | ( (((x) >> 24) & 0xff) )
+#else
+#define SWAPBYTES(x) ( x )
+#endif
+#define bswap(x) (SWAPBYTES(x))
+#endif
+
+
+// Default implementation
+
+C_RESULT video_zeromem32( uint32_t* dst, uint32_t length )
+{
+  while( length )
+  {
+    *dst = 0;
+
+    dst ++;
+    length--;
+  }
+
+  return C_OK;
+}
+
+C_RESULT video_copy32(uint32_t* dst, uint32_t* src, uint32_t nb)
+{
+  uint32_t i;
+
+  for( i = 0; i < nb; i++ )
+  {
+    dst[i] = src[i];
+  }
+
+  return C_OK;
+}
+
+C_RESULT video_copy32_swap(uint32_t* dst, uint32_t* src, uint32_t nb)
+{
+  uint32_t i;
+
+  for( i = 0; i < nb; i++ )
+  {
+    dst[i] = bswap( src[i] );
+  }
+
+  return C_OK;
+}
